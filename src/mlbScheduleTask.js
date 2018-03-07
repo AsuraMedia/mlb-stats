ea5b6d8a0aa48eca953a1b4baced510856a97e06
Oscar Diaz <odiaz.dw@gmail.com>
2018-03-07 13:00:22 -0600

    added date schedule tasker with Momentjs

--- src/mlbScheduleTask.js
+++ src/mlbScheduleTask.js
@@ -2,27 +2,51 @@ import * as Rx from 'rxjs';
 import { MlbMapReducer } from './mlbMapReducer';
 import { config } from './mlbApiConfig';
 
+const moment = require('moment');
+
 export class MlbScheduleTask {
 
     constructor () {
 
     }
 
-    getXmlData () {
+    getXmlData ( days ) {
 
         const mapReducer = new MlbMapReducer()
-        const date = { year: '2017', month: '05', day: '13' }
 
-        return mapReducer.mapSchedule( date )
-            .then( ( scheduleResult ) => {
-                return scheduleResult.map( ( schedule ) => {
-                    return {
-                        xmlUrl: `${schedule.path}`
-                    }
-                } )
-            } )
+        Rx.Observable.from( days ).zip(
+            Rx.Observable.interval(1000), ( day ) => {
+
+                console.log( 'MONTH ------------------>', day.format("dddd, MMMM Do YYYY, h:mm:ss a") )
+                
+            })
+            .subscribe()
+
+        // return mapReducer.mapSchedule( date )
+        //     .then( ( scheduleResult ) => {
+        //         return scheduleResult.map( ( schedule ) => {
+        //             return {
+        //                 date: date,
+        //                 xmlUrl: `${schedule.path}`
+        //             }
+        //         } )
+        //     } )
 
     }
+
+    getDaysArrayByMonth( date ) {
+        var momentDate = moment(`${date.year}-${date.month}`, "YYYY-MM")
+        var daysInMonth = momentDate.daysInMonth();
+        var arrDays = [];
+      
+        while(daysInMonth) {
+          var current = moment(`${date.year}-${date.month}`, "YYYY-MM").date(daysInMonth);
+          arrDays.push(current);
+          daysInMonth--;
+        }
+
+        return arrDays;
+      }
 
 }
 
--- src/test.js
+++ src/test.js
@@ -18,121 +18,139 @@ import {
  import { mode } from './math'
 
 //xml test here
-const mlbService = new MlbService()
-const mlbScheduleTask = new MlbScheduleTask()
-mlbScheduleTask.getXmlData()
-    .then((urlList) => {
+// const mlbService = new MlbService()
+// const mlbScheduleTask = new MlbScheduleTask()
+// mlbScheduleTask.getXmlData()
+//     .then((urlList) => {
 
-        const url = urlList[1].xmlUrl
-        mlbService.getXml(url)
-            .then((response) => {
-                if (response.status !== 200) {
-                    //
-                } else {
+//         const url = urlList[1].xmlUrl
+//         mlbService.getXml(url)
+//             .then((response) => {
+//                 if (response.status !== 200) {
+//                     //
+//                 } else {
 
-                    const fs = require('file-system');
+//                     const fs = require('file-system');
 
-                    let eventDataSet: Array<EventRow> = [];
-                    let inningsArray: Array<Inning> = [];
-                    let pitchesDataPerAtBat = {};
+//                     let eventDataSet: Array<EventRow> = [];
+//                     let inningsArray: Array<Inning> = [];
+//                     let pitchesDataPerAtBat = {};
 
-                    const parseString = require('xml2js').parseString
-                    parseString(response.data, (err, result) => {
-                        inningsArray = result.game.inning;
-                    });
+//                     const parseString = require('xml2js').parseString
+//                     parseString(response.data, (err, result) => {
+//                         inningsArray = result.game.inning;
+//                     });
 
-                    const calculateAvgPitchVelocity = ( pitchType, pitchesDataPerAtBat ) => {
-                        let count = 0;
-                        let totalSum = 0;
-                        for ( let key in pitchesDataPerAtBat ) {
-                            count += pitchesDataPerAtBat[key][pitchType+'count'];
-                            totalSum += pitchesDataPerAtBat[key][pitchType+'speedTotal'];
-                        }
-                        return totalSum / count
-                    }
+//                     const calculateAvgPitchVelocity = ( pitchType, pitchesDataPerAtBat ) => {
+//                         let count = 0;
+//                         let totalSum = 0;
+//                         for ( let key in pitchesDataPerAtBat ) {
+//                             count += pitchesDataPerAtBat[key][pitchType+'count'];
+//                             totalSum += pitchesDataPerAtBat[key][pitchType+'speedTotal'];
+//                         }
+//                         return totalSum / count
+//                     }
 
-                    const calculateAvgPitchZone = ( pitchType, pitchesDataPerAtBat ) => {
-                        let zones = []
-                        for ( let key in pitchesDataPerAtBat ) {
-                           zones = zones.concat( pitchesDataPerAtBat[key][pitchType+'zones'] )
-                        }
-                        return mode( zones )
-                    }
+//                     const calculateAvgPitchZone = ( pitchType, pitchesDataPerAtBat ) => {
+//                         let zones = []
+//                         for ( let key in pitchesDataPerAtBat ) {
+//                            zones = zones.concat( pitchesDataPerAtBat[key][pitchType+'zones'] )
+//                         }
+//                         return mode( zones )
+//                     }
 
-                    inningsArray.forEach( ( inning: Inning ) => {
-                        inning.top.forEach( ( topHalf ) => {
-                            topHalf.atbat.forEach( ( atbat, abIndex ) => {
-                                atbat.pitch.forEach( ( pitch ) => {
+//                     inningsArray.forEach( ( inning: Inning ) => {
+//                         inning.top.forEach( ( topHalf ) => {
+//                             topHalf.atbat.forEach( ( atbat, abIndex ) => {
+//                                 atbat.pitch.forEach( ( pitch ) => {
 
-                                    console.log(pitch.$.pitch_type)
+//                                     console.log(pitch.$.pitch_type)
                                     
-                                    if ( !pitchesDataPerAtBat[atbat.$.play_guid] ) {
-                                        pitchesDataPerAtBat[atbat.$.play_guid] = { 
-                                            FFcount: 0, 
-                                            FFspeedTotal: 0,
-                                            FTcount: 0,
-                                            FTspeedTotal: 0,
-                                            CHcount: 0,
-                                            CHspeedTotal: 0,
-                                            CUcount: 0,
-                                            CUspeedTotal: 0,
-                                            SLcount: 0,
-                                            SLspeedTotal: 0,
-                                            KCcount: 0,
-                                            KCspeedTotal: 0,
-                                            SIcount: 0,
-                                            SIspeedTotal: 0,
-                                            FFzones: [],
-                                            FTzones: [],
-                                            CHzones: [],
-                                            CUzones: [],
-                                            SLzones: [],
-                                            KCzones: [],
-                                            SIzones: []
-                                        };
-                                    }
+//                                     if ( !pitchesDataPerAtBat[atbat.$.play_guid] ) {
+//                                         pitchesDataPerAtBat[atbat.$.play_guid] = { 
+//                                             FFcount: 0, 
+//                                             FFspeedTotal: 0,
+//                                             FTcount: 0,
+//                                             FTspeedTotal: 0,
+//                                             CHcount: 0,
+//                                             CHspeedTotal: 0,
+//                                             CUcount: 0,
+//                                             CUspeedTotal: 0,
+//                                             SLcount: 0,
+//                                             SLspeedTotal: 0,
+//                                             KCcount: 0,
+//                                             KCspeedTotal: 0,
+//                                             SIcount: 0,
+//                                             SIspeedTotal: 0,
+//                                             FFzones: [],
+//                                             FTzones: [],
+//                                             CHzones: [],
+//                                             CUzones: [],
+//                                             SLzones: [],
+//                                             KCzones: [],
+//                                             SIzones: []
+//                                         };
+//                                     }
 
-                                    // Average pitch speed
-                                    pitchesDataPerAtBat[atbat.$.play_guid][pitch.$.pitch_type+'count']++
-                                    pitchesDataPerAtBat[atbat.$.play_guid][pitch.$.pitch_type+'speedTotal'] += Math.round(parseFloat(pitch.$.start_speed))
+//                                     // Average pitch speed
+//                                     pitchesDataPerAtBat[atbat.$.play_guid][pitch.$.pitch_type+'count']++
+//                                     pitchesDataPerAtBat[atbat.$.play_guid][pitch.$.pitch_type+'speedTotal'] += Math.round(parseFloat(pitch.$.start_speed))
 
-                                    // Average pitch zone
-                                    pitchesDataPerAtBat[atbat.$.play_guid][pitch.$.pitch_type+'zones'].push(pitch.$.zone)
+//                                     // Average pitch zone
+//                                     pitchesDataPerAtBat[atbat.$.play_guid][pitch.$.pitch_type+'zones'].push(pitch.$.zone)
                                     
-                                } )
-                                eventDataSet.push({
-                                    inningNo: inning.$.num,
-                                    opponent: inning.$.home_team,
-                                    isHome: false,
-                                    batterHeight: atbat.$.b_height,
-                                    batterHand: atbat.$.stand,
-                                    pitcherHand: atbat.$.p_throws,
-                                    homeTeamRuns: atbat.$.home_team_runs,
-                                    awayTeamRuns: atbat.$.away_team_runs,
-                                    runnersOn: atbat.runner ? atbat.runner.length : 0,
-                                    avgFFspeed: calculateAvgPitchVelocity( 'FF', pitchesDataPerAtBat ),
-                                    avgFFzone: calculateAvgPitchZone( 'FF', pitchesDataPerAtBat ),
-                                    event: atbat.$.event
-                                })
-                            } )
-                        })
-                    })
+//                                 } )
+//                                 eventDataSet.push({
+//                                     inningNo: inning.$.num,
+//                                     opponent: inning.$.home_team,
+//                                     isHome: false,
+//                                     batterHeight: atbat.$.b_height,
+//                                     batterHand: atbat.$.stand,
+//                                     pitcherHand: atbat.$.p_throws,
+//                                     homeTeamRuns: atbat.$.home_team_runs,
+//                                     awayTeamRuns: atbat.$.away_team_runs,
+//                                     runnersOn: atbat.runner ? atbat.runner.length : 0,
+//                                     avgFFspeed: calculateAvgPitchVelocity( 'FF', pitchesDataPerAtBat ),
+//                                     avgFFzone: calculateAvgPitchZone( 'FF', pitchesDataPerAtBat ),
+//                                     event: atbat.$.event
+//                                 })
+//                             } )
+//                         })
+//                     })
                     
-                    console.log( 'DATA SET ------->', JSON.stringify( eventDataSet ) )
+//                     console.log( 'DATA SET ------->', JSON.stringify( eventDataSet ) )
+    //             }
+    //         })
 
-                    const converter = require('json-2-csv');
-                    const options = {
-                        checkSchemaDifferences: false
-                    }
+    // })
 
-                    converter.json2csv(eventDataSet, (err, csv) => {
-                        console.log(csv)
-                        fs.writeFile('eventDataSet.csv', csv, (err) => {
-                            console.log(err)
-                        })
-                    }, options )
-                }
-            })
+    const mlbService = new MlbService()
+    const mlbScheduleTask = new MlbScheduleTask()
 
-    })
+    const date = { year: '2017', month: '05' }
 
+    const days = mlbScheduleTask.getDaysArrayByMonth( date );
+    mlbScheduleTask.getXmlData( days )
+
+    // mlbScheduleTask.getXmlData( date )
+    //     .then((urlList) => { 
+
+    //         console.log(urlList)
+
+
+
+    //         // mlbService.getXml(url)
+    //         //     .then((response) => {
+    //         //         if (response.status !== 200) {
+    //         //             //
+    //         //         } else { 
+    //         //             //
+    //         //         }
+    //         //     })
+
+    //     })
+
+    // const fs = require('file-system');
+    // fs.writeFile("xmlTest.xml", xml, err => {
+    //     console.log(err);
+    // });
